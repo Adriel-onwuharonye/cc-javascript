9cb2ef7d604342cb9d80b3b69e48547aaf53f4df
adriel onwuharonye <onwuharonyeadriel@gmail.com>
2024-10-08 14:20:35 +0100

    learn about vriables and operators

---
+++ variable.js
@@ -0,0 +1,45 @@
+alert('hello')
+// this is a multiline comment this is a multi;line comment
+
+let fname = "adriel";
+// alert(fname);
+fname='first place';
+// alert(fname);
+
+const surname='onwuharonye';
+// alert(surname);
+surname='gfam';
+// alert(surname);
+
+let firstNum=5;
+let secondNum=2;
+let add = firstNum+secondNum;
+console.log(add);
+let subtract=firstNum-secondNum;
+let divide = first /secondNum;
+let multiply = first * secondNum;
+console.log('5-2='+ subtract)
+console.log('5/2='+ divide)
+console.log('5*2='+ multiply)
+let power = firstNum ** secondNum;
+console.log('5 raised to power 2='+ power);
+let remainder = firstNum%secondNum;
+console.log('5 divided by 2 is 2 r '+ remainder);
+console.log('5>2='=(firstNum>secondNum))
+console.log('5<2='=(firstNum>=secondNum));
+console.log('5<=2='=(firstNum>=secondNum));
+console.log('5 is equal to 5'+(firstNum>=secondNum));
+console.log('5'==5);
+console,log('5'==5);
+console,log('3 is not equal to 2'+ (firstNum !=secondNum))
+
+let colorOFTop='white'
+let colorOFLegs='ash'
+let length=1.5;
+let isFoldable=true;
+let materialOFTop='plasticc';
+let materialOFLegs='aluminium';
+let isHeavy=false;
+let texture = 'coarse'
+let noOFTables=6;
+
